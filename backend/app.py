35da4895fedb26e55c2971b7d9717243f75ed16c
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-07-04 12:16:55 +0300

    auth & validation

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,97 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import db, Book
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin Resource Sharing
-
-# Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_recommendations.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+CORS(app)  
 
-db.init_app(app)
+#
 
-# Routes
+
+
+# Define routes and endpoints here
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    
+    # TODO: Implement user registration logic
+    
+    return jsonify(message='User registered successfully')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    
+    # TODO: Implement user login logic
+    
+    return jsonify(message='User logged in successfully')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    # TODO: Implement user logout logic
+    
+    return jsonify(message='User logged out successfully')
+
+
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = Book.query.all()
-    book_list = [{'id': book.id, 'title': book.title, 'author': book.author, 'description': book.description} for book in books]
-    return jsonify(book_list)
+    # TODO: Implement logic to fetch all books
+    
+    return jsonify(books=[])
 
-# Add more routes as needed
+@app.route('/books', methods=['POST'])
+def create_book():
+    data = request.get_json()
+    title = data['title']
+    author = data['author']
+    description = data['description']
+    
+    # TODO: Implement logic to create a new book
+    
+    return jsonify(message='Book created successfully')
 
-# Create database tables if they don't exist
-@app.cli.command()
-def create_db():
-    with app.app_context():
-        db.create_all()
-    print('Database tables created')
+@app.route('/books/<book_id>', methods=['PUT'])
+def update_book(book_id):
+    data = request.get_json()
+    title = data['title']
+    author = data['author']
+    description = data['description']
+    
+    # TODO: Implement logic to update the book with the given book_id
+    
+    return jsonify(message='Book updated successfully')
+
+@app.route('/books/<book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    # TODO: Implement logic to delete the book with the given book_id
+    
+    return jsonify(message='Book deleted successfully')
+
+@app.route('/books/<book_id>/reviews', methods=['POST'])
+def add_review(book_id):
+    data = request.get_json()
+    rating = data['rating']
+    comment = data['comment']
+    
+    # TODO: Implement logic to add a review for the book with the given book_id
+    
+    return jsonify(message='Review added successfully')
+
+@app.route('/books/<book_id>/reviews', methods=['GET'])
+def get_reviews(book_id):
+    # TODO: Implement logic to fetch reviews for the book with the given book_id
+    
+    return jsonify(reviews=[])
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    # Add other user fields as needed
+    password = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    books = db.relationship('Book', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,5 +14,11 @@ class Book(db.Model):
     author = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('books', lazy=True))
-    # Add other book fields as needed
+    reviews = db.relationship('Review', backref='book', lazy=True)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Float, nullable=False)
+    comment = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
